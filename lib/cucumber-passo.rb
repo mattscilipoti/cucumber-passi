3a805f1de0fa8cfbdb0a6b6c937a1f0da1a22b2f
Matt Scilipoti <matt@scilipoti.name>
2011-09-17 09:19:34 -0400

    bundle gem cucumber-passo

---
+++ lib/cucumber-passo.rb
@@ -0,0 +1,7 @@
+require "cucumber-passo/version"
+
+module Cucumber
+  module Passo
+    # Your code goes here...
+  end
+end
---
+++ lib/cucumber-passo/version.rb
@@ -0,0 +1,5 @@
+module Cucumber
+  module Passo
+    VERSION = "0.0.1"
+  end
+end
