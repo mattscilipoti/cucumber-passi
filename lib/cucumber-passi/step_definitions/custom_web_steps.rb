5f9cc3dc7fadb6ae68e6fb1e59b3a5ea78182410
Matt Scilipoti <matt@scilipoti.name>
2011-11-03 22:28:27 -0400

    Update step defs for cucumber 1.2
    tableish was deprecated.  Can't say I agree.
    Use #step instead of Given/When/Then inside steps.

--- lib/cucumber-passi/step_definitions/custom_web_steps.rb
+++ lib/cucumber-passi/step_definitions/custom_web_steps.rb
@@ -30,17 +30,17 @@ When /^I (follow|press) "([^"]+)" for #{requested_model_pattern}$/ do |action, f
 
   mut = model.send(default_finder(model), record_identifier)
   model_selector = '#' + dom_id(mut)
-  When %{I #{action} "#{field}" within "#{model_selector}"}
+  step %{I #{action} "#{field}" within "#{model_selector}"}
 end
 
 # Alias for 'Then I should be on'
 Then /^I should be viewing (.+)$/ do |page_name|
-  Then %(I should be on #{page_name})
+  step %(I should be on #{page_name})
 end
 
 # Alias for 'Then I should be on'
 Then /^I should be editing (.+)$/ do |page_name|
-  Then %(I should be on the "edit" page for #{page_name})
+  step %(I should be on the "edit" page for #{page_name})
 end
 
 Then /^I should see the following:$/ do |table|
@@ -49,7 +49,7 @@ Then /^I should see the following:$/ do |table|
     label = find_label(field)
     raise "Could not find label '#{field}'" unless label
     element_id = label.native.attributes['for'].value
-    Then %(I should see "#{value}" within "##{element_id}")
+    step %(I should see "#{value}" within "##{element_id}")
     #page.should have_css("##{element_id}", value)
   end
 end
@@ -89,5 +89,5 @@ end
 # Sounds better for:
 #    When I am viewing the emails for Project:A
 When /^I am viewing (.+)$/ do |page_name|
-  When %(I am on #{page_name})
+  step %(I am on #{page_name})
 end
